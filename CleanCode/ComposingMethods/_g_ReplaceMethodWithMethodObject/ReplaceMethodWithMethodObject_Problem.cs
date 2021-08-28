8b77b4b1b9d7874efbcb3e7190425b208ab95ed4
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-08-28 15:14:48 +0700

    update ReplaceMethodWithMethodObject

--- CleanCode/ComposingMethods/_g_ReplaceMethodWithMethodObject/ReplaceMethodWithMethodObject_Problem.cs
+++ CleanCode/ComposingMethods/_g_ReplaceMethodWithMethodObject/ReplaceMethodWithMethodObject_Problem.cs
@@ -8,5 +8,17 @@ namespace CleanCode.ComposingMethods._g_ReplaceMethodWithMethodObject
 {
     class ReplaceMethodWithMethodObject_Problem
     {
-    }
+		class Order
+		{
+			public double price()
+			{
+				double primaryBasePrice = 1;
+				double secondaryBasePrice = 1;
+				double tertiaryBasePrice = 1;
+				// Perform long computation.
+
+				return tertiaryBasePrice * secondaryBasePrice * primaryBasePrice;
+			}
+		}
+	}
 }
--- CleanCode/ComposingMethods/_g_ReplaceMethodWithMethodObject/ReplaceMethodWithMethodObject_Solution.cs
+++ CleanCode/ComposingMethods/_g_ReplaceMethodWithMethodObject/ReplaceMethodWithMethodObject_Solution.cs
@@ -8,5 +8,30 @@ namespace CleanCode.ComposingMethods._g_ReplaceMethodWithMethodObject
 {
     class ReplaceMethodWithMethodObject_Solution
     {
-    }
+		class Order
+		{
+			// ...
+			public double price()
+			{
+				return new PriceCalculator(this).compute();
+			}
+		}
+		class PriceCalculator
+		{
+			private double primaryBasePrice;
+			private double secondaryBasePrice;
+			private double tertiaryBasePrice;
+			public PriceCalculator(Order order)
+			{
+				// Copy relevant information from the
+				// order object.
+			}
+
+			public double compute()
+			{
+				return primaryBasePrice * secondaryBasePrice * tertiaryBasePrice;
+				// Perform long computation.
+			}
+		}
+	}
 }
