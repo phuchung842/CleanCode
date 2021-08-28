210b064702e796791d05a3a2091430d21d1f5648
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-08-28 15:13:16 +0700

    update ExtractVariable

--- CleanCode/ComposingMethods/_c_ExtractVariable/ExtractVariable_Problem.cs
+++ CleanCode/ComposingMethods/_c_ExtractVariable/ExtractVariable_Problem.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace CleanCode.ComposingMethods._c_ExtractVariable
 {
+    //You have an expression that's hard to understand.
     class ExtractVariable_Problem
     {
+        string platform = "MAC";
+        string browser = "IE";
+        int resize;
+        private bool wasInitialized()
+        {
+            return false;
+        }
+        void renderBanner()
+        {
+            if ((platform.ToUpper().IndexOf("MAC") > -1)
+                    && (browser.ToUpper().IndexOf("IE") > -1)
+                    && wasInitialized() && resize > 0)
+            {
+                // do something
+            }
+        }
     }
 }
--- CleanCode/ComposingMethods/_c_ExtractVariable/ExtractVariable_Solution.cs
+++ CleanCode/ComposingMethods/_c_ExtractVariable/ExtractVariable_Solution.cs
@@ -8,5 +8,23 @@ namespace CleanCode.ComposingMethods._c_ExtractVariable
 {
     class ExtractVariable_Solution
     {
+        string platform = "MAC";
+        string browser = "IE";
+        int resize;
+        private bool wasInitialized()
+        {
+            return false;
+        }
+        void renderBanner()
+        {
+            bool isMacOs = platform.ToUpper().IndexOf("MAC") > -1;
+            bool isIE = browser.ToUpper().IndexOf("IE") > -1;
+            bool wasResized = resize > 0;
+
+            if (isMacOs && isIE && wasInitialized() && wasResized)
+            {
+                // do something
+            }
+        }
     }
 }
