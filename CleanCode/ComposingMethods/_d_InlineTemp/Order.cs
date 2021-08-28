d29264f52da203fb368bb44edb3217f118ae787c
Phúc Hưng Ngô Phương <phuchung842@outlook.com.vn>
2021-08-28 15:11:34 +0700

    create Order in InlineTemp

---
+++ CleanCode/ComposingMethods/_d_InlineTemp/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCode.ComposingMethods._d_InlineTemp
+{
+    class Order
+    {
+        public double basePrice()
+        {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+    }
+}
